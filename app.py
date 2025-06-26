4d1f34b5e011fdfd3910b7ed98e2fed47e167ac1
AKERELE Ifedayo David <ifedayoa278@gmail.com>
2025-06-26 15:56:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -178,5 +178,8 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
