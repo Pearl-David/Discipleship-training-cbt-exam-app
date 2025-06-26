dfa1f6a2aba0768ba0fd71f92c40130e7e43aab9
AKERELE Ifedayo David <ifedayoa278@gmail.com>
2025-06-26 15:57:06 -0700

    Update app.py

--- cbt-app/app.py
+++ cbt-app/app.py
@@ -204,5 +204,8 @@ def admin_dashboard():
     users = User.query.all()
     return render_template('admin.html', users=users)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
